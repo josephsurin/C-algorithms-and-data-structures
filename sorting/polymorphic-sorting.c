27e78da541b74d248a802891a56d4ed4d187de52
Joseph Surin <joseph.surin@gmail.com>
2019-07-09 17:32:36 +1000

    polymorphic merge sort

--- sorting/polymorphic-sorting.c
+++ sorting/polymorphic-sorting.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <assert.h>
 
+#define MIN(x,y) ((x) > (y) ? (y) : (x))
+
 typedef struct animal_s animal_t;
 void print_animals(animal_t*, int n);
 int cmp_by_id(void*, void*);
@@ -55,6 +57,53 @@ void* selection_sort(void* A, int n, size_t size, int cmp(void*, void*)) {
     return A;
 }
 
+void* merge_sort(void* A, int n, size_t size, int cmp(void*, void*)) {
+    char* merge(void* A1, void* A2, int n1, int n2) {
+        char* merged_arr = (char*)malloc((n1+n2)*size);
+        char *cA1 = A1, *cA2 = A2;
+        assert(merged_arr != NULL);
+        int i = 0;
+        while(n1 && n2) {
+            if(cmp(cA1, cA2) <= 0) {
+                memcpy(merged_arr+i*size, cA1, size);
+                cA1 += size; n1--;
+            } else {
+                memcpy(merged_arr+i*size, cA2, size);
+                cA2 += size; n2--;
+            }
+            i++;
+        }
+        if(n1) {
+            while(n1) {
+                memcpy(merged_arr+i*size, cA1, size);
+                cA1 += size; i++; n1--;
+            }
+        } else if(n2) {
+            while(n2) {
+                memcpy(merged_arr+i*size, cA2, size);
+                cA2 += size; i++; n2--;
+            }
+        }
+        return merged_arr;
+    }
+    char* cA = A;
+    char* partitions_buffer = (char*)calloc(n, size);
+    for(int i = 0; i < n; i++) memcpy(partitions_buffer+i*size, cA+i*size, size);
+    for(int partition_size = 1; partition_size < n; partition_size *= 2) {
+        for(int i = 0; i < n; i += 2*partition_size) {
+            if(i+partition_size >= n) break;
+            int partition_size2 = MIN(n-i-partition_size, partition_size);
+            char* merged = merge(partitions_buffer+i*size, partitions_buffer+(i+partition_size)*size, partition_size, partition_size2);
+            int k = 0;
+            for(int j = i; j < i+2*partition_size; j++) {
+                memcpy(partitions_buffer+j*size, merged+k*size, size);
+                k++;
+            }
+        }
+    }
+    return partitions_buffer;
+}
+
 struct animal_s {
     int db_id;
     char name[30];
@@ -68,6 +117,7 @@ int main(void) {
     printf("[ * ] Insertion Sort: "); print_animals(insertion_sort(copy_array(animals, N, S), N, S, cmp_by_id), N);
     printf("[ * ] Bubble Sort: "); print_animals(bubble_sort(copy_array(animals, N, S), N, S, cmp_by_id), N);
     printf("[ * ] Selection Sort: "); print_animals(selection_sort(copy_array(animals, N, S), N, S, cmp_by_id), N);
+    printf("[ * ] Merge Sort: "); print_animals(merge_sort(copy_array(animals, N, S), N, S, cmp_by_id), N);
     return 0;
 }
 
